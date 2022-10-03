b2f284534608723debdec3149393a473b533bcb8
hwl17648 <1764836685@qq.com>
2022-10-03 16:26:24 +0800

    push commit

--- src/main/java/com/hwl/GitTest.java
+++ src/main/java/com/hwl/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hot-fix commit");
         System.out.println("master marge");
         System.out.println("hot-fix marge");
+        System.out.println("push commit");
     }
 }
